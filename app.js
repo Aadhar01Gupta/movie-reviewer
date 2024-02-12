23b67c17ccbd152d156b94dadb47274b1b669a46
Aadhar Gupta <105646656+Aadhar01Gupta@users.noreply.github.com>
2024-02-12 14:50:42 +0530

    more changes

--- app.js
+++ app.js
@@ -1,44 +1,27 @@
 const apiKey = "https://www.omdbapi.com/?&apikey=31103aae&i=";
 const searchKey = "https://www.omdbapi.com/?&apikey=31103aae&s=";
 
-function home() {
-  let Home_page_id = [
-    "tt3896198",
-    "tt1201607",
-    "tt7286456",
-    "tt0371746",
-    "tt1517268",
-    "tt15398776",
-    "tt6723592",
-    "tt6751668",
-  ];
-  getapi(Home_page_id);
-}
 
-window.onload = function () {
-  home();
+window.onload = () => {
+  getapi();
 };
 
-async function getapi(key) {
-  const homeData = JSON.parse(localStorage.getItem('homeData'))
+async function getapi() {
+  const homeData = JSON.parse(sessionStorage.getItem('homeData'));
   if (homeData === null) {
     var dataKey = [];
     const articleType = await fetch('https://www.omdbapi.com/?&apikey=31103aae&s=marvel&type=series');
     var abc = await articleType.json();
-    debugger
     console.log(abc);
-
     dataKey.push(abc.Search);
-    const newData =   dataKey.flat(1)
-
+    const newData = dataKey.flat(1);
     console.log(newData);
     const productstemp = document.getElementById("products-template").innerHTML;
     const productscompile = Handlebars.compile(productstemp);
     const compileddata = productscompile({ data: newData });
     const container = document.getElementById("container");
     container.innerHTML = compileddata;
-    $(".card").on("click", function abc(e) {
-
+    $(".card").on("click", (e) => {
       window.scrollTo({
         top: 0,
         behavior: "smooth",
@@ -47,19 +30,17 @@ async function getapi(key) {
       sec.setAttribute("style", "display:grid");
       const load = document.getElementById("loading-animation");
       load.setAttribute("style", "display:block");
-      $("body").toggleClass("stop-scrolling");
+      $("body").addClass("stop-scrolling");
       popup(e.currentTarget.id);
     });
-    localStorage.setItem('homeData', JSON.stringify(newData));
-  }
-  else {
+    sessionStorage.setItem('homeData', JSON.stringify(newData));
+  } else {
     const productstemp = document.getElementById("products-template").innerHTML;
     const productscompile = Handlebars.compile(productstemp);
     const compileddata = productscompile({ data: homeData });
     const container = document.getElementById("container");
     container.innerHTML = compileddata;
-    $(".card").on("click", function abc(e) {
-
+    $(".card").on("click", (e) => {
       window.scrollTo({
         top: 0,
         behavior: "smooth",
@@ -68,40 +49,43 @@ async function getapi(key) {
       sec.setAttribute("style", "display:grid");
       const load = document.getElementById("loading-animation");
       load.setAttribute("style", "display:block");
-      $("body").toggleClass("stop-scrolling");
+      $("body").addClass("stop-scrolling");
       popup(e.currentTarget.id);
     });
   }
-
 }
 
 function popup(data) {
-  setTimeout(async function () {
+  setTimeout(async () => {
     const articleType = await fetch(`${apiKey}${data}`);
     var abc = await articleType.json();
     const load = document.getElementById("loading-animation");
     load.setAttribute("style", "display:none");
     console.log(abc);
-    const productstemp =
-      document.getElementById("products-template2").innerHTML;
+    const productstemp = document.getElementById("products-template2").innerHTML;
     const productscompile = Handlebars.compile(productstemp);
     const compileddata = productscompile({ data: abc });
     const container = document.getElementById("pop");
     container.innerHTML = compileddata;
     const popup = document.getElementById("pop");
     popup.setAttribute("style", "display:flex");
-
-    $("#close").on("click", function () {
+    $("#close").on("click", () => {
       const target = document.getElementById("lol");
       target.setAttribute("src", "#");
       const popup = document.getElementById("pop");
       popup.setAttribute("style", "display:none");
       const sec = document.getElementById("secondary");
       sec.setAttribute("style", "display:none");
-      $("body").toggleClass("stop-scrolling");
+      $("body").removeClass("stop-scrolling");
+    });
+    $(document).on("keydown", (e) => {
+      if (e.key === "Escape") {
+        $("#close").click();
+      }
     });
   }, 300);
 }
+
 function debounce(func, delay) {
   let timeoutId;
   return function (...args) {
@@ -115,16 +99,15 @@ function debounce(func, delay) {
 
 const debouncedSearch = debounce(searchFnc, 1000);
 
-$("#search").on("input", function (e) {
+$("#search").on("input", (e) => {
   debouncedSearch(e.currentTarget.value);
 });
 
-$("#search").on("keydown", function (e) {
-  if(e.key === 'Enter'){
+$("#search").on("keydown", (e) => {
+  if (e.key === 'Enter') {
     searchFnc(e.currentTarget.value);
   }
 });
-
 
 async function searchFnc(data) {
   debugger;
@@ -143,7 +126,7 @@ async function searchFnc(data) {
   container.innerHTML = compileddata;
   const sug = document.getElementById("sug");
   sug.innerText = "Search Results";
-  $(".card").on("click", function abc(e) {
+  $(".card").on("click", (e) => {
     window.scrollTo({
       top: 0,
       left: 100,
@@ -153,21 +136,18 @@ async function searchFnc(data) {
     sec.setAttribute("style", "display:grid");
     const load = document.getElementById("loading-animation");
     load.setAttribute("style", "display:block");
-    $("body").toggleClass("stop-scrolling");
-    setTimeout(function () {
+    $("body").addClass("stop-scrolling");
+    setTimeout(() => {
       popup(e.currentTarget.id);
     }, 500);
   });
 }
 
-$("#home").on("click", function () {
+$("#home").on("click", () => {
   home();
   document.getElementById("search").value = "";
 });
 
-Handlebars.registerHelper("splitStringToArray", function (string) {
+Handlebars.registerHelper("splitStringToArray", (string) => {
   return string.split(",");
 });
-
-
-
